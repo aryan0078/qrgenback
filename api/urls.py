36d59036d949511f5fc84bbe8e5e7d86cd93da20
Aryan <aryan8931080555@gmail.com>
2020-06-03 16:12:36 +0530

    FIx

--- api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path,include
-from . import views
-urlpatterns=[
-    path('login/',views.login,name="login"),
-    path('register/',views.signup,name="register")
-]
--- userauth/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-class Users(models.Model):
-    email = models.CharField(max_length=60,unique=True)
-    firstname=models.CharField(max_length=60,default=False)
-    lastname=models.CharField(max_length=60,default=False)
-    password=models.CharField(max_length=60,default=False)
-    timezone=models.CharField(max_length=60,default=False)
-    language=models.CharField(max_length=60,default=False)
-    
