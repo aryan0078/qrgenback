f58c0d9a33f27f3e64095144314e6ff92c52290b
Aryan <aryan8931080555@gmail.com>
2020-06-03 15:10:36 +0530

    Fix

--- api/models.py
+++ api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
-# Create your models here.
+class Users(models.Model):
+    email = models.CharField(max_length=60,unique=True)
+    firstname=models.CharField(max_length=60,default=False)
+    lastname=models.CharField(max_length=60,default=False)
+    password=models.CharField(max_length=60,default=False)
+    timezone=models.CharField(max_length=60,default=False)
+    language=models.CharField(max_length=60,default=False)
+    
